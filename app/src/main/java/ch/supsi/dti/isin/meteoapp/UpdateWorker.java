084613374cdf0d8198fc844ba25cdf3f7bf84b51
Matteo Miggiano <matteo.miggiano@student.supsi.ch>
2021-04-30 13:07:20 +0200

    doc

--- app/src/main/java/ch/supsi/dti/isin/meteoapp/UpdateWorker.java
+++ app/src/main/java/ch/supsi/dti/isin/meteoapp/UpdateWorker.java
@@ -9,6 +9,7 @@ import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -35,31 +36,32 @@ public class UpdateWorker extends Worker {
             List<Location> updatedList = LocationsHolder.get(getApplicationContext()).getLocations();
             updateLocationInfoTask.execute(updatedList).get();
 
-            StringBuilder stringBuilder = new StringBuilder();
 
+            int counter = 0;
             for (Location location : updatedList){
+                StringBuilder stringBuilder = new StringBuilder();
+
                 if(location.getTemp_min() < TEMP_MIN)
                     stringBuilder.append(location.getName().toUpperCase() + " :\n " + "min_temp = " + location.getTemp_min() + "\n");
 
                 if(location.getTemp_min() > TEMP_MAX)
                     stringBuilder.append(location.getName().toUpperCase() + " :\n " + "max_temp = " + location.getTemp_max() + "\n");
-            }
 
-            if(stringBuilder.length() > 0){
-                NotificationCompat.Builder mBuilder = null;
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                    mBuilder = new NotificationCompat.Builder(getApplicationContext(),"default")
-                            .setSmallIcon(android.R.drawable.ic_menu_day)
-                            .setContentTitle(LocalTime.now().toString())
-                            .setContentText(stringBuilder.toString())
-                            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                if(stringBuilder.length() > 0) {
+                    NotificationCompat.Builder mBuilder = null;
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                        mBuilder = new NotificationCompat.Builder(getApplicationContext(), "default")
+                                .setSmallIcon(android.R.drawable.ic_menu_info_details)
+                                .setContentTitle(LocalTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME))
+                                .setContentText(stringBuilder.toString())
+                                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                    }
+
+                    NotificationManagerCompat managerCompat = NotificationManagerCompat.from(getApplicationContext());
+                    managerCompat.notify(counter, mBuilder.build());
+                    counter++;
                 }
-
-                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(getApplicationContext());
-                managerCompat.notify(0, mBuilder.build());
             }
-
-
             return Result.success();
         } catch (ExecutionException | InterruptedException e) {
             e.printStackTrace();
